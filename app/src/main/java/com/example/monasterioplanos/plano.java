d1357f032e52ab1b125936fce02a7e5e46902f59
KEVIN ALONSO MAMANI CONDORI <kmamanicondo@unsa.edu.pe>
2024-10-29 19:21:53 -0500

    Agregando proyecto

---
+++ app/src/main/java/com/example/monasterioplanos/plano.java
@@ -0,0 +1,137 @@
+package com.example.monasterioplanos;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class plano extends View {
+
+    private Paint paintWall, paintBorde,paintSeleccion, paintWindow, paintText;
+
+    public plano(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+
+        // Configuración del pincel para las paredes y los cuadros
+        paintWall = new Paint();
+        paintWall.setColor(Color.LTGRAY);
+        paintWall.setStyle(Paint.Style.FILL);
+        paintWall.setStrokeWidth(8f);
+
+        //Para el borde
+        paintBorde= new Paint();
+        paintBorde.setColor(Color.LTGRAY);
+        paintBorde.setStyle(Paint.Style.STROKE);
+        paintBorde.setStrokeWidth(8f);
+
+        // Configuración de area definida para seleccionar
+        paintSeleccion= new Paint();
+        paintSeleccion.setColor(Color.TRANSPARENT);
+        paintSeleccion.setStyle(Paint.Style.STROKE);
+        paintSeleccion.setStrokeWidth(4f);
+
+        // Configuración del pincel del circulo celeste
+        paintWindow = new Paint();
+        paintWindow.setColor(Color.CYAN);
+        paintWindow.setStyle(Paint.Style.FILL);
+
+        // Paint para texto
+        paintText = new Paint();
+        paintText.setColor(Color.BLACK);
+        paintText.setTextSize(15f);
+        paintText.setAntiAlias(true);
+
+    }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Ajustar escala
+        int width = getWidth();
+        int height = getHeight();
+        float scaleFactor = Math.min(width / 600f, height / 1000f);
+        canvas.scale(scaleFactor, scaleFactor);
+
+        //Borde del plano
+        canvas.drawRect(50, 20, 550, 1100, paintBorde);
+
+        //Areas definidas para seleccionar
+        canvas.drawRect(410, 40, 540, 110, paintSeleccion);//Area Clautro Novicias
+        canvas.drawRect(260, 40, 390, 110, paintSeleccion);//Area Patio del silencio
+        canvas.drawRect(260, 140, 340, 220, paintSeleccion);//Area Claustro de los naranjos
+        canvas.drawRect(130, 140, 190, 350, paintSeleccion);//Area Claustro Mayor
+        canvas.drawRect(200, 110, 250, 350, paintWall); //Area Pinacoteca
+        canvas.drawRect(50, 110, 120, 350, paintWall);  //Area CoroBajo
+        canvas.drawRect(50, 350, 120, 800, paintWall); //Area Iglesia de santa catalina
+        canvas.drawRect(340, 230, 550, 550, paintWall); //Area Nuevo Monasterio
+        canvas.drawCircle(138, 565, 15, paintWindow); //Area de la pileta
+        canvas.drawRect(130, 600, 170, 800, paintSeleccion); //Area de calle Granada
+        canvas.drawRect(190, 600, 300, 800, paintSeleccion); //Area calle Toledo
+        canvas.drawRect(140, 810, 300, 860, paintSeleccion);//Area Calle Sevilla
+        canvas.drawRect(250, 350, 300, 500, paintSeleccion);//Area Calle Cordova
+
+
+        //El resto de sin funcionalidad
+        canvas.drawRect(50, 20, 550, 55, paintWall);
+        canvas.drawRect(325, 110, 550, 210, paintWall);
+        canvas.drawRect(50, 110, 300, 140, paintWall);
+        canvas.drawRect(500, 20, 550, 210, paintWall);
+        canvas.drawRect(380, 20, 430, 100, paintWall);
+        canvas.drawRect(200, 110, 250, 350, paintWall);
+        canvas.drawRect(270, 230, 550, 550, paintWall);
+        canvas.drawRect(150, 370, 250, 550, paintWall);
+        canvas.drawRect(150, 580, 250, 800, paintWall);
+        canvas.drawRect(150, 850, 250, 980, paintWall);
+        canvas.drawRect(270, 850, 400, 1020, paintWall);
+        canvas.drawRect(270, 500, 400, 800, paintWall);
+
+
+        //A partir de aqui solo se definio los textos
+        //Claustro Noivias
+        canvas.drawText("Claustro", 435, 80, paintText);
+        canvas.drawText("Novicias", 435, 100, paintText);
+
+        //Patio del silencio
+        canvas.drawText("Patio del", 300, 80, paintText);
+        canvas.drawText("Silencio", 305, 100, paintText);
+        //Claustro Mayor
+        canvas.drawText("Claustro", 130, 250, paintText);
+        canvas.drawText("Mayor", 130, 270, paintText);
+        //Claustro de los naranjos
+        canvas.drawText("Claustro", 260, 170, paintText);
+        canvas.drawText("de los", 260, 190, paintText);
+        canvas.drawText("Naranjos", 260, 210, paintText);
+        //Nuevo Monasterio
+        canvas.drawText("Nuevo Monasterio", 360, 380, paintText);
+        //Calle sevilla
+        canvas.drawText("Calle sevilla", 160, 830, paintText);
+
+        //A partir de aqui los textos tienen rotacion
+        canvas.rotate(90, 75, 130);
+        //coro Bajo
+        canvas.drawText("Coro Bajo", 75, 130, paintText);
+        //Iglesia de Santa Cartalina
+        canvas.drawText("Iglesia Santa Catalina", 400, 130, paintText);
+        //Pinacoteca
+        canvas.drawText("Pinacoteca", 150, -15, paintText);
+        //Calle cordova
+        canvas.drawText("Calle cordova", 300, -50, paintText);
+        //Plaza zocodober
+        //Calle granada
+        canvas.drawText("Calle Granada", 600, 70, paintText);
+        //Cale toledo
+        canvas.drawText("Calle toledo", 650, -50, paintText);
+
+
+    }
+}
